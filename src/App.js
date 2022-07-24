db32c50cfc77eb0fbce27fdf0f29e6671121c78a
Safi <safikurrahman7514@gmail.com>
2022-07-24 14:06:45 +0600

    add aos animation

--- src/App.js
+++ src/App.js
@@ -1,8 +1,14 @@
 import Navbar from "./components/Navbar";
 import { Routes, Route } from 'react-router-dom';
 import { publicRoute } from './routes/PublicRoute';
+import { useEffect } from 'react';
+import AOS from 'aos';
+import 'aos/dist/aos.css';
 
 function App() {
+  useEffect(() => {
+    AOS.init();
+  }, [])
   return (
     <>
       <Navbar>
--- src/pages/Home/Landing.js
+++ src/pages/Home/Landing.js
@@ -4,12 +4,12 @@ const Landing = () => {
     return (
         <>
             <div class="hero bg-accent min-h-[60vh]">
-                <div class="hero-content flex-col lg:flex-row max-w-6xl mx-auto md:px-16 ">
+                <div class="hero-content flex-col lg:flex-row max-w-6xl mx-auto md:pt-20 ">
                     <div>
-                        <h2 className='font-bold text-primary text-lg uppercase'>best cleaning services</h2>
-                        <h1 class="lg:text-5xl md:text-4xl sm:text-3xl font-bold capitalize">professional <span className='text-primary'>cleaning</span> services</h1>
+                        <h2 data-aos="fade-right" data-aos-duration="2000" className='font-bold text-primary text-lg uppercase'>best cleaning services</h2>
+                        <h1 data-aos="fade-right" data-aos-duration="3000" class="lg:text-5xl md:text-4xl sm:text-3xl font-bold capitalize">professional <span className='text-primary'>cleaning</span> services</h1>
                         <p class="py-6">Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem quasi. In deleniti eaque aut repudiandae et a id nisi.</p>
-                        <button class="btn btn-primary">Get Started</button>
+                        <button data-aos="fade-right" data-aos-duration="4000" class="btn btn-primary">Get Started</button>
                     </div>
                     <img src="https://i.ibb.co/PrtKD4L/banner-bg.png" alt='banner image' className='h-full' />
                 </div>
